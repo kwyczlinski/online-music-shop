8516590a4f1986ade944f1d870f08a0591f6fa07
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-01-25 16:17:17 +0100

    Add failing test for signed order

--- tests/unit/test_order_create.py
+++ tests/unit/test_order_create.py
@@ -3,6 +3,28 @@ from src.order import Order
 class TestOrderCreation:
 
     def test_order_creation(self):
-        order = Order("Happier Than Ever")
+        order = Order("Happier Than Ever", "test@example.com")
+
         assert order.product == "Happier Than Ever"
         assert order.status == "pending"
+    
+    def test_empty_email(self):
+        order = Order("Happier Than Ever", "")
+
+        assert order.email == "Invalid"
+
+    def test_empty_email(self):
+        order = Order("Happier Than Ever", "")
+
+        assert order.email == "Invalid"
+
+    def test_no_at_in_email(self):
+        order = Order("Happier Than Ever", "example.email")
+
+        assert order.email == "Invalid"
+
+
+    def test_no_dot_after_at_in_email(self):
+        order = Order("Happier Than Ever", "test@example")
+
+        assert order.email == "Invalid"
