350ea145de42107f7547a224a5c4d7668891a07e
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-01-25 22:39:50 +0100

    Implement status change

--- src/order.py
+++ src/order.py
@@ -5,3 +5,11 @@ class Order:
         self.product = product if isinstance(product, str) else None
         self.status = "pending"
         self.email = "Invalid" if not re.match(r"[^@]+@[^@]+\.[^@]+", email)  else email
+
+    def change_status(self):
+        if self.status == "pending":
+            self.status = "ready"
+        elif self.status == "ready":
+            self.status = "shipping"
+        elif self.status == "shipping":
+            self.status = "collected"
