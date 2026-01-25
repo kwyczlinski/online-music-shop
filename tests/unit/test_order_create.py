9c253f50654cae65864213fa56d9eb6c53ccb72b
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-01-25 22:21:39 +0100

    Add failing tests for incorrect product

--- tests/unit/test_order_create.py
+++ tests/unit/test_order_create.py
@@ -1,7 +1,7 @@
 from src.order import Order
 import pytest
 
-class TestOrderCreation:
+class TestOrder:
 
     @pytest.mark.parametrize("product, email, expected_product, expected_email", [
         ("Happier Than Ever", "test@example.com", "Happier Than Ever", "test@example.com"),
@@ -10,6 +10,12 @@ class TestOrderCreation:
         ("Happier Than Ever", "example.com", "Happier Than Ever", "Invalid"),
         ("Happier Than Ever", "@example.com", "Happier Than Ever", "Invalid"),
         ("Happier Than Ever", "test@example", "Happier Than Ever", "Invalid"),
+        ("Happier Than Ever", "test@example.com", "Happier Than Ever", "test@example.com"),
+        (None, "test@example.com", None, "test@example.com"),
+        (True, "test@example.com", None, "test@example.com"),
+        (False, "test@example.com", None, "test@example.com"),
+        ({}, "test@example.com", None, "test@example.com"),
+
     ], ids=[
         "succesfull ordercreation",
         "different product",
@@ -17,6 +23,11 @@ class TestOrderCreation:
         "no at in email",
         "email starts with at",
         "no dot after at in email",
+        "product is None",
+        "product is True",
+        "product is False",
+        "product undefined",
+        "product is empty object",
     ])
     def test_order_creation(self, product, email, expected_product, expected_email):
         order = Order(product, email)
