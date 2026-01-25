7545dc80cc45e074350f9c4da3dc1f592405bec2
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-01-26 00:56:23 +0100

    Add failing tests for digital orders

---
+++ tests/unit/test_digital_order.py
@@ -0,0 +1,27 @@
+import pytest
+from datetime import datetime, timezone, timedelta
+
+class TestDigitalOrder:
+
+    @pytest.fixture
+    def order(self):
+        order = DigitalOrder("Happier Than Ever", "test@example.com")
+        return order
+
+    @pytest.mark.parametrize("starting_status, collect_time, expected_status, succeeded",  [
+        ("collected", datetime.now(timezone.utc), "collected", False),
+        ("collected", datetime.now(timezone.utc) - timedelta(days=1), "collected", False),
+        ("collected", datetime.now(timezone.utc) - timedelta(days=14), "collected", False),
+    ], ids=[
+        "order just collected",
+        "order collected day ago",
+        "order collected two weeks ago",
+    ])
+    def test_order_return(self, order, starting_status, collect_time, expected_status, succeeded):
+        order.status = starting_status
+        order.collected_at = collect_time
+
+        success = order.file_return()
+
+        assert order.status == expected_status
+        assert success == succeeded
